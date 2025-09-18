57a33bac71c89e642be4ddc62408f41e4306dec9
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2025-09-18 22:55:35 +0530

    Longest subarray with optimal solution

---
+++ LonSubArrayOptimal.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int LonSubArray(vector <int> &arr, long long k){
+    int left =0;
+    int right =0;
+    long long sum = arr[0];
+    int maxlen= 0;
+    int n = arr.size();
+    while(right <n){
+        while(left<=right && sum>k){
+            sum-=arr[left];
+            left++;
+        }
+        if(sum==k){
+            maxlen = max(maxlen, right-left+1);
+        }
+        right ++;
+        if(right <n){
+            sum += arr[right];
+        }
+    }
+    return maxlen;
+}
+int main(){
+    int n;
+    cout<<"Enter the size of the array"<<endl;
+    cin>>n;
+    long long k;
+    cout<<"Enter the sum"<<endl;
+    cin>>k;
+    vector <int> arr(n);
+    for(int i =0; i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<LonSubArray(arr,k);
+}
