81365060d79b554b8a72b1703bb6d1acabd38570
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2025-09-19 21:18:01 +0530

    Better solution

---
+++ TwoSumBetterSolution.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+string TwoSum(vector <int> arr, int n, int target){
+    map <int ,int> mpp;
+    for(int i =0;i<n; i++){
+        int element = arr[i];
+        int rem = target - element;
+        if(mpp.find(rem)!=mpp.end()){
+            return "Yes";   // for index return{mpp[rem],i}
+        }
+        mpp[element]= i;
+    }
+    return "No";
+}
+int main(){
+    int n;
+    cout<<"Enter the size of the array"<<endl;
+    cin>>n;
+    int target;
+    cout<<"Enter the targeted value"<<endl;
+    cin>>target;
+    vector <int> arr(n);
+    cout<<"Enter the array elements"<<endl;
+    for (int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<TwoSum(arr,n,target);
+    return 0;
+}
