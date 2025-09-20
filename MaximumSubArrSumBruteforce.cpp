ec1436d6bd131ff6cf524a376292c27e79cd0b3d
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2025-09-20 23:34:54 +0530

    Brute force Approach

---
+++ MaximumSubArrSumBruteforce.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int MaximumSubarraySum (vector <int> &arr, int n){
+   int  maxi = INT_MIN;
+   for(int i =0; i<n; i++){
+    for(int j = i;j <n; j++){
+        int sum = 0;
+        for (int k =i; k<=j ; k++){
+          sum += arr[k];
+          maxi = max(sum, maxi);
+        }
+    
+    }
+   }
+   return maxi;
+}
+int main(){
+    int n;
+    cout<<"Enter the size of the array"<<endl;
+    cin>>n;
+    vector <int> arr(n);
+    cout<<"enter the array element"<<endl;
+    for(int i =0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<MaximumSubarraySum(arr,n);
+    return 0;
+}
+//TC -- nearly O(n^3)
+//SC -- O(1)
+ 
