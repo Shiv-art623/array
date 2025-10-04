c020d0d5df6b2d718e574186b130d960800ae6cf
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2025-10-04 20:35:52 +0530

    Printing Nth row Brute force

--- PrintRowOfPascal'sTriangleBF.cpp
+++ PrintRowOfPascal'sTriangleBF.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 #include<algorithm>
 using namespace std;
-long long  ReturnElement(int n, int r){
+
+long long  FunNCR(int n, int r){
     long long ans = 1;
-    cout<<ans;
     for(int i =0; i<r; i++){
         ans = ans*(n- i);
         ans = ans/(i+1);
@@ -11,12 +11,12 @@ long long  ReturnElement(int n, int r){
         return ans;
 }
 int main(){
-    int n ;
+    int row ;
     cout<<"enter the row no."<<endl;
-    cin>>n;
-    int r;
-    cout<<"enter the column no."<<endl;
-    cin>>r;
-    cout<<ReturnElement(n-1,r-1);
-    return 0;
+    cin>>row;
+    for(int i = 0 ; i<=row-1; i++){
+        cout<<FunNCR(row-1, i);
+    }
+    return 0 ;
 }
+//TC - O(n*r)
