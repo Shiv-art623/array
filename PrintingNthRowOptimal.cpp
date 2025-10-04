1a2fdb34d3b9a61a00574becc2331e9a59d7092b
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2025-10-04 20:43:21 +0530

    Optimal

---
+++ PrintingNthRowOptimal.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+ int main(){
+  int row;
+  cin>>row;
+  int ans =1;
+  cout<<ans;
+  for(int i = 1; i<row; i++){
+    ans = ans*(row-i);
+    ans = ans/i;
+    cout<<ans;
+  }
+  return 0;
+ }
