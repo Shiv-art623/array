39cb59959e749cb1493fc54b172fa24d12fd0701
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2025-10-06 23:33:14 +0530

    Better solution

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+
+vector<vector<int>> ThreeSum(vector<int> &arr, int n){
+     set<vector<int>> st;
+     for(int i =0; i<n; i++){
+        set<int> hashset;
+        for(int j =i+1; j<n; j++){
+            int third = -(arr[i]+arr[j]);
+            if(hashset.find(third)!= hashset.end()){
+                vector<int> temp = {arr[i],arr[j],third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
+            }
+            hashset.insert(arr[j]);
+            
+        }
+     }
+     vector<vector<int>> ans(st.begin(), st.end());
+    
+     
+     return ans;
+}
+int main(){
+    int n; cin>>n;
+    vector<int> arr(n);
+    for(int i =0; i<n; i++){
+        cin>>arr[i];
+    }
+  vector<vector<int>> ans = ThreeSum(arr,n);
+  for(int i =0; i<ans.size(); i++){
+    for(int j =0; j<ans.size(); j++){
+        for(int k =0; k<ans.size(); k++){
+        cout<<ans[i][j][k]<<" ";
+    }
+}
+    cout<<endl;
+  }
+  return 0;
+    
